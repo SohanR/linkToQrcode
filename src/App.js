c41109b83e5b7d0c7c0d7d40ac93353c88fe68f9
sohanr <mizan.rahman66d@gmail.com>
2023-01-20 18:58:34 +0600

    structured

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Button, CircularProgress, Container, InputLabel, NativeSelect, TextField, Typography } from '@mui/material';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+    <Container align='center'  >
+      <Typography align='center' variant='h3' mb={10} mt={10} color='secondary'   > 
+        Link To QR Code Generator
+      </Typography>
+
+      <TextField id="outlined-basic" type={URL} margin='normal' label="Enter URL" variant="outlined" />
+
+     
+        <InputLabel style={{marginTop:20, marginBottom:10}} variant="standard" htmlFor="uncontrolled-native">
+          Size
+        </InputLabel>
+        <NativeSelect
+        style={{marginBottom:10}}
+          variant='filled'
+          defaultValue={300}
+          inputProps={{
+            name: 'size',
+            id: 'uncontrolled-native',
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <option value={100}>Small</option>
+          <option value={300}>Medium</option>
+          <option value={700}>Large</option>
+        </NativeSelect>
+   
+        <Button variant="contained" color='secondary' style={{display:'flex', marginTop:20}} >
+           Generate
+      </Button>
+
+      <CircularProgress style={{margin:150}} color="secondary" />
+
+
+      <Button variant="contained" color='secondary' style={{display:'flex', marginTop:20}} >
+            Download Image
+      </Button>
+    </Container>
+      
+
   );
 }
 
